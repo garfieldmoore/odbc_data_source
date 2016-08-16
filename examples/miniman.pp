dc4b0af48abbc1668ad2987d8025af3b9b7bcc2f
garfield <garfield.moore@rbi.co.uk>
2016-08-16 15:55:06 +0100

    corrected comments

--- examples/miniman.pp
+++ examples/miniman.pp
@@ -1,7 +1,7 @@
 # the minimum required is the database name and database server name/IP
 # This will create an ODBC datda source with folloing properties;
 # sql_version => 'SQLSERVER'
-# dsn_65bit => true
+# dsn_64bit => true
 # user_id => 'Administrator'
 # description => ''
 # trusted_connection => 'Yes'
